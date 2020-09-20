65df15c50a89f42e4efa187987f74656a636b117
philip <pjohansson01@yahoo.com>
2020-09-20 23:19:18 +0200

    working on shit

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# DevToolsNizer
+### DevToolsNizer
+
+
+
+
+## get-all
+
+# exampel
+get-all users -j 
+
+# params
+* -j - out put in js format
+
+
+## clear-all
+
+# exampel
+clear-all users
+
+# params
+* -p - prints res
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from mongodb import Mongodb
 from dotenv import load_dotenv
 from funktions import Functions
+import os
 load_dotenv()
 
 connect = Mongodb()
 func = Functions(connect)
 
-while(1):
+while 1:
     input_data = input(":")
     input_data_array = input_data.split(' ')
     cmd_head = input_data_array[0]
@@ -15,5 +16,10 @@ while(1):
 
     print(cmd_head, cmd_str, cmd_args)
 
-    if(cmd_head == "get-all"):
+    if cmd_head == "get-all":
         func.get_all(cmd_str, cmd_args)
+    elif cmd_head == "clear-all":
+        if func.alert_comfirm():
+            func.clear_all(cmd_str, cmd_args)
+    elif cmd_head == "sys-clear":
+        os.system("clear")
